f3b6a0ef61d0f50129a5e7ded4e9a1ae65057103
OhSuBin13 <dhtnqls0103@naver.com>
2025-05-10 00:02:04 +0900

    Controller 수정

--- src/main/java/inha/primero_server/domain/inquiry/controller/InquiryController.java
+++ src/main/java/inha/primero_server/domain/inquiry/controller/InquiryController.java
@@ -2,19 +2,21 @@ package inha.primero_server.domain.inquiry.controller;
 
 import inha.primero_server.domain.inquiry.service.InquiryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/inquiry")
 @RequiredArgsConstructor
 public class InquiryController {
 
     private final InquiryService inquiryService;
-
+    /*
     @GetMapping("/test")
-    public String test() {
-        inquiryService.saveInquiry();
-        return "succeed";
-    }
+    public ResponseEntity<> inquiryListPage(Model model,)
+    */
 }
