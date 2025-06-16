584a36d8ae7f7a0f2c1ef5df797b8403e0b56574
mo_o.noe <hyeonhoi11@naver.com>
2025-06-16 19:34:48 +0900

    feat: 실제 이메일 발송 로직 구현

---
+++ src/main/java/inha/primero_server/domain/auth/EmailSender.java
@@ -0,0 +1,30 @@
+package inha.primero_server.domain.auth;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailSender {
+    private final JavaMailSender javaMailSender;
+
+    public void send(String to, String subject, String content) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true); // HTML 가능
+            helper.setFrom("hyeonhoi11@naver.com");
+
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("이메일 전송 실패", e);
+        }
+    }
+}
